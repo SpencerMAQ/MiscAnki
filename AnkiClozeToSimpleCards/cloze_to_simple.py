a2b49aa7b35b357c4b2a95478f531c29ec7d73c0
Michael Spencer Quinto <SpencerMAQ@users.noreply.github.com>
2019-03-14 21:03:44 +0800

    getting there

--- AnkiClozeToSimpleCards/cloze_to_simple.py
+++ AnkiClozeToSimpleCards/cloze_to_simple.py
@@ -1,2 +1,21 @@
-sample_text = '{{c1::AAA}} - bbb c {{c2::dsadsa}} dasdasdasd {{c2::ddddddd}} ddsadasdsa {{c3::dasdsadasd}} dddd'
+sample_text = '{{c1::AAA}} - bbb c {{c2::dsadsa}} dasdasdasd {{c2::ddddddd}} ddsadasdsa {{c3::dasdsadasd}} dddd {{c4::dasdsadasd}}'
 
+import re
+
+pattern = '[c0-9]'
+
+x = re.findall(pattern, sample_text)
+# remove extraneous leading 'c'
+x = [int(i) for i in x if i!='c']
+max_cloze_num = max(x)
+
+print(x)
+print(max_cloze_num)
+
+refined_text = ''
+
+for word in sample_text.split(' '):
+    word = word.strip('{').strip('}')
+    refined_text += word + ' '
+
+# print(refined_text)
